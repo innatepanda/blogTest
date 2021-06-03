573bff64eab38d1ce2f36936601cded2380a86e0
virtualpanda <1905228@kiit.ac.in>
2021-06-03 21:07:38 +0530

    working

--- finaltest/src/component/Config/firebase-admin.js
+++ finaltest/src/component/Config/firebase-admin.js
@@ -1,7 +1,17 @@
+var admin = require("firebase-admin");
 
-import  admin from 'firebase-admin';
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./key.json");
+
+// Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
-    
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://blogtest-14ada-default-rtdb.firebaseio.com/'
 });
 
-export default admin
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
