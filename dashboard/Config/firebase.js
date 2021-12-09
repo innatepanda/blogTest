fc8dc7f2fcb1fe6b3277d5e80e34a8e7fba8ef6f
virtualpanda <1905228@kiit.ac.in>
2021-12-09 19:17:31 +0530

    save point, idk if things work

---
+++ dashboard/Config/firebase.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+import firebase from 'firebase'
+var firebaseConfig = {
+  apiKey: "AIzaSyCRJxdzuodYh3i3eydNsAE8xFjxSHWdW0w",
+  authDomain: "blogtest-14ada.firebaseapp.com",
+  databaseURL: "https://blogtest-14ada-default-rtdb.firebaseio.com",
+  projectId: "blogtest-14ada",
+  storageBucket: "blogtest-14ada.appspot.com",
+  messagingSenderId: "975556708270",
+  appId: "1:975556708270:web:1920a6bc25386faae0aa3f",
+  measurementId: "G-8P3KEKQ2EM"
+};
+firebase.initializeApp(firebaseConfig);
